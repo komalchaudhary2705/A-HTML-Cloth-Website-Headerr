428cb48e618f676199153b8397cd6ab9282e7dd0
komalkalsoom1 <komalchaudhary2705@gmail.com>
2026-02-08 08:56:28 +0500

    add

---
+++ assets/js/script.js
@@ -0,0 +1,32 @@
+const navbar = document.querySelector(".navbar");
+const openBtn = document.querySelector(".navbar-open-btn");
+const closeBtn = document.querySelector(".nav-close-btn");
+const overlay = document.querySelector(".overlay");
+const header = document.querySelector(".header");
+
+/* Open Navbar */
+openBtn.addEventListener("click", () => {
+  navbar.classList.add("active");
+  overlay.classList.add("active");
+});
+
+/* Close Navbar */
+closeBtn.addEventListener("click", () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+/* Close on Overlay Click */
+overlay.addEventListener("click", () => {
+  navbar.classList.remove("active");
+  overlay.classList.remove("active");
+});
+
+/* Sticky Shadow on Scroll */
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 50) {
+    header.classList.add("active");
+  } else {
+    header.classList.remove("active");
+  }
+});
